5cb82c55680ea9773521b2f5aa06836215bc8f64
BlacksmithTimmet <f.berdel@gmx.de>
2018-09-04 15:57:56 +0200

    Add files via Upload
    First Exercise with composer

---
+++ Exercise01/Classes/exercise01.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fberdel
+ * Date: 04.09.18
+ * Time: 15:53
+ */
+
+namespace BlacksmithTimmet\ComposerExercise\Classes;
+
+public function helloComposer(){
+    echo "Hello Composer";
+    return true;
+}
